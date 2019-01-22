a33e6908cf6f562980620106e3e2b9eb0ad71866
Michael Green <me@michaelgreen.net>
2019-01-21 16:27:05 -0800

    Fix spacing issues with flexbox

--- src/components/item.js
+++ src/components/item.js
@@ -10,10 +10,12 @@ export default class Item extends Component {
                 <span onClick={()=>{this.props.upvote(this.props.id)}} className="vote noselect">⬆</span>
                 <span onClick={()=>{this.props.downvote(this.props.id)}} className="vote noselect">⬇</span>
                 </div>
-                <a href={this.props.link}>{this.props.title}</a>
-                <span className="backlink"> ({this.props.link})</span>
-                <div className="votes">{this.props.votes} points</div>
-                <div className="date"><Moment fromNow>{this.props.time.toString()}</Moment></div>
+                <div>
+                    <a href={this.props.link}>{this.props.title}</a>
+                    <span className="backlink"> ({this.props.link})</span>
+                    <div className="votes">{this.props.votes} points</div>
+                    <div className="date"><Moment fromNow>{this.props.time.toString()}</Moment></div>
+                </div>
             </div>
         )
     }
